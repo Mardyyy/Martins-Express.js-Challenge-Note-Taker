15036d80453bf6914c7a2d00c13f79f364d70f96
Mardyyy <martinapopot@gmail.com>
2023-07-10 11:59:35 -0500

    Added full working application w/ readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Martins-Express.js-Challenge-Note-Taker
-Your challenge is to create an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+## Description
+This project is to create an application called Note Taker that can be used to write and save notes. This project creates an application where a user can organize my thoughts and keep track of tasks I need to complete. This project allowed me to apply and learn about Express.js back end and how to save and retrieve note data from a JSON file.
+
+## Table of Contents 
+- [Description](#description)
+- [Web Link](#web-link)
+- [Screenshots](#screenshots)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Web Link
+
+<a href="https://martins-not-taker-5e7f5384cf74.herokuapp.com/notes">Note taker link</a>
+
+## Screenshots
+
+<img src="./public/assets/images/index-html.PNG" alt="Screen Shot of index.html"/>
+<img src="./public/assets/images/start-notes-html.PNG" alt="Screen Shot of starting notes.html"/>
+<img src="./public/assets/images/writen-notes.PNG" alt="Screen Shot of notes.html w/ example notes"/>
+
+## Credits
+
+Starter Code:
+<a href="https://github.com/coding-boot-camp/miniature-eureka">Starter Code Source</a>
+
+## License
+
+None
+
+## Questions
+
+For any additional questions, reach me at <a href="martinapopot@gmail.com">My email</a> or <a href="https://github.com/mardyyy">My github</a>.
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile} = require('./helpers/fsUtils');
 
 
 // Middleware for parsing JSON and urlencoded form data
@@ -22,14 +22,14 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// Wildcard route to direct users to a homepage
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 // GET Route for retrieving all the notes
-app.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+// app.get('/api/notes', (req, res) => {
+//     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+// });
+// GET Route for retrieving all the notes
+app.get('/api/notes', async(req, res) => {
+    const data = await readFromFile('./db/db.json')
+    res.json(JSON.parse(data))
 });
 
 // POST Route for submitting notes
@@ -41,7 +41,7 @@ app.post('/api/notes', (req, res) => {
         const newNotes = {
             title,
             text,
-            title_id: uuidv4(),
+            id: uuidv4(),
         };
 
         readAndAppend(newNotes, './db/db.json');
@@ -55,6 +55,27 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.json('Error in posting notes');
     }
+});
+
+app.delete('/api/notes/:id', async(req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const data = await readFromFile('./db/db.json')
+    const json = await JSON.parse(data)
+    const result = json.filter((note) => note.id !== id);
+    console.log(result);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${id} has been deleted 🗑️`);
+
+})
+
+// Wildcard route to direct users to a homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // Listening
